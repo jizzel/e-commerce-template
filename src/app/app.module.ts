945f0ca515113856cedc198ac62ed74b6095b6f0
Joseph Afriyie Attakorah <juxluvjoe@gmail.com>
2018-12-04 12:32:28 +0300

    upgraded to Angular7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,10 @@ import { EcommerceComponent } from './layout/ecommerce/ecommerce.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import {APP_ROUTES} from "./app.routing";
-import {RouterModule} from "@angular/router";
+import {APP_ROUTES} from './app.routing';
+import {RouterModule} from '@angular/router';
 import { FixedpluginComponent } from './components/fixedplugin/fixedplugin.component';
-import {SharedModuleModule} from "./shared-module/shared-module.module";
+import {SharedModuleModule} from './shared-module/shared-module.module';
 import { ParallaxComponent } from './layout/parallax/parallax.component';
 
 
